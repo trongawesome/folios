1418b7e37d234d18663da6e85e4a36220d576e80
tantrongtt <tantrongtt@gmail.com>
2019-11-24 17:53:14 +0800

    add sitemap (#6)

--- @narative/gatsby-theme-novela/gatsby-config.js
+++ @narative/gatsby-theme-novela/gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = ({
     `gatsby-transformer-remark`,
     `gatsby-transformer-yaml`,
     `gatsby-plugin-theme-ui`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-feed`,
       options: {
