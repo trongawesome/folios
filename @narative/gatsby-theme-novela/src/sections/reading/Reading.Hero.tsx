e98e8b6e503f05cf77d894068916621e736aadef
Trong Nguyen <tantrongtt@gmail.com>
2019-12-25 18:29:16 +0800

    update cover style

--- @narative/gatsby-theme-novela/src/sections/reading/Reading.Hero.tsx
+++ @narative/gatsby-theme-novela/src/sections/reading/Reading.Hero.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+import Section from "@components/Section";
 import Headings from '@components/Headings';
+import HorizontalRule from '@components/HorizontalRule';
 import Image, { ImagePlaceholder } from '@components/Image';
 
 import mediaqueries from '@styles/media';
-import { IArticle, IAuthor } from '@types';
+import { IArticle } from '@types';
 
 interface ArticleHeroProps {
   article: IArticle;
@@ -18,83 +20,56 @@ const ReadingHero: React.FC<ArticleHeroProps> = ({ article }) => {
     article.hero.full.constructor === Object;
 
   return (
-    <Hero>
-      <Header>
-        <HeroHeading>{article.title}</HeroHeading>
-        <Excerpt>{article.excerpt}</Excerpt>
-      </Header>
-      <HeroImage id="ReadingImage__Hero">
-        {hasHeroImage ? (
-          <Image src={article.hero.full} />
-        ) : (
-          <ImagePlaceholder />
-        )}
-      </HeroImage>
-    </Hero>
+    <Section>
+      <HeroContainer>
+        <HeroImage id="ReadingImage__Hero">
+          {hasHeroImage ? (
+            <Image src={article.hero.full} />
+          ) : (
+            <ImagePlaceholder />
+          )}
+        </HeroImage>
+        <HeroTextContainer>
+          <Author>{article.author}</Author>
+          <HeroHeading>{article.title}</HeroHeading>
+          <Excerpt>{article.excerpt}</Excerpt>
+        </HeroTextContainer>
+      </HeroContainer>
+      <HorizontalRule />
+    </Section>
   );
 };
 
 export default ReadingHero;
 
-const Hero = styled.div`
-  ${p => mediaqueries.phablet`
-    &::before {
-      content: "";
-      width: 100%;
-      height: 20px;
-      background: ${p.theme.colors.primary};
-      position: absolute;
-      left: 0;
-      top: 0;
-      transition: ${p.theme.colorModeTransition};
-    }
+const HeroContainer = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 64px;
+  z-index: 1;
+  margin-top: 100px;
+  margin-bottom: 80px;
 
-    &::after {
-      content: "";
-      width: 100%;
-      height: 10px;
-      background: ${p.theme.colors.background};
-      position: absolute;
-      left: 0;
-      top: 10px;
-      border-top-left-radius: 25px;
-      border-top-right-radius: 25px;
-      transition: ${p.theme.colorModeTransition};
-    }
+  ${mediaqueries.tablet`
+    grid-template-columns: 1fr;
   `}
 `;
 
-const Header = styled.header`
+const HeroTextContainer = styled.header`
   position: relative;
   z-index: 10;
-  margin:100px auto 56px;
-  padding-left: 68px;
-  max-width: 749px;
+  margin-top: 100px;
+  align-self: start;
 
-  ${mediaqueries.desktop`
-    padding-left: 53px;
-    max-width: calc(507px + 53px);
-    margin: 100px auto 70px;
-  `}
+`;
 
-  ${mediaqueries.tablet`
-    padding-left: 0;
-    margin: 100px auto 70px;
-    max-width: 480px;
-  `}
-
-  ${mediaqueries.phablet`
-    margin: 64px auto 64px;
-    padding: 0 40px;
-  `}
-
-  @media screen and (max-height: 700px) {
-    margin: 100px auto 48px;
-  }
+const Author = styled(Headings.h4)`
+  margin-bottom: 8px;
 `;
 
 const HeroHeading = styled(Headings.h1)`
-  font-size: 48px;
+  font-size: 56px;
   font-family: ${p => p.theme.fonts.title};
   margin-bottom: 25px;
   font-weight: bold;
@@ -102,7 +77,7 @@ const HeroHeading = styled(Headings.h1)`
 
   ${mediaqueries.tablet`
     margin-bottom: 20px;
-    font-size: 36px;
+    font-size: 48px;
   `}
 
   ${mediaqueries.phablet`
@@ -111,13 +86,13 @@ const HeroHeading = styled(Headings.h1)`
 `;
 
 const Excerpt = styled(Headings.h3)`
-  font-size: 28px;
+  font-size: 20px;
   font-family: ${p => p.theme.fonts.body};
   color: ${p => p.theme.colors.secondary};
   margin-bottom: 24px;
   margin-top: 24px;
   font-weight: normal;
-  line-height: 1.5;
+  line-height: 1.65;
 
   ${mediaqueries.tablet`
   `}
@@ -129,23 +104,58 @@ const Excerpt = styled(Headings.h3)`
 
 const HeroImage = styled.div`
   position: relative;
-  z-index: 1;
+  height: auto;
+  transition: transform 0.3s var(--ease-out-quad),
+  box-shadow 0.3s var(--ease-out-quad);
+  box-shadow:
+    0px 1px 2px rgba(0, 0, 0, 0.07),
+    0px 2px 4px rgba(0, 0, 0, 0.07),
+    0px 4px 8px rgba(0, 0, 0, 0.07),
+    0px 8px 16px rgba(0, 0, 0, 0.07),
+    0px 16px 32px rgba(0, 0, 0, 0.07),
+    0px 32px 64px rgba(0, 0, 0, 0.07);
   width: 100%;
-  max-width: 944px;
-  overflow: hidden;
-  margin: 0 auto;
 
-  ${mediaqueries.tablet`
-    max-width: 100%;
-  `}
+  & > div {
+    height: 100%;
 
-  ${mediaqueries.phablet`
-    margin: 0 auto;
-    width: calc(100vw - 40px);
-    height: 220px;
-
-    & > div {
-      height: 220px;
+    ::before {
+      background: linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.3) 90%);
+      box-shadow: 1px 0 rgba(0, 0, 0, 0.1), 3px 0 0px rgba(255, 255, 255, 0.1);
+      content: "";
+      display: block;
+      height: 100%;
+      position: absolute;
+      width: 10px;
+      z-index: 2;
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+      border-top-right-radius: 2px;
+      border-bottom-right-radius: 2px;
     }
-`}
+  }
+
+  ::before {
+    background-image: url(/utils/shadow.png);
+    background-size: contain;
+    background-position: right;
+    background-repeat: no-repeat;
+    content: " ";
+    display: block;
+    height: 100%;
+    right: 99%;
+    position: absolute;
+    top: 0;
+    transform-origin: center right;
+    width: 100%;
+    z-index: 0;
+    pointer-events: none;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    object-position: center;
+  }
 `;
