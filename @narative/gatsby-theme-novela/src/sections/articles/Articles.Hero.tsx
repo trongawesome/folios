05b3afc1f6da8db2babf61f62b6ce46554140fce
tantrongtt <tantrongtt@gmail.com>
2020-01-21 08:09:10 +0800

    add archive page

--- @narative/gatsby-theme-novela/src/sections/articles/Articles.Hero.tsx
+++ @narative/gatsby-theme-novela/src/sections/articles/Articles.Hero.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import styled from '@emotion/styled';
+import { Link } from 'gatsby';
 
 import Section from '@components/Section';
 import mediaqueries from '@styles/media';
@@ -49,6 +50,9 @@ const ArticlesHero: React.FC<IAuthor> = ({ authors }) => {
         <HeroHeading dangerouslySetInnerHTML={{ __html: hero.writingHeading }} />
         <InfoText>
           Without words, apps would be an unusable jumble of shapes and icons.
+          <Anchor to="/archive" data-a11y="false">
+            Archive
+          </Anchor>
         </InfoText>
       </HeadingContainer>
     </Section>
@@ -98,3 +102,14 @@ const InfoText = styled.p`
   color: ${p => p.theme.colors.secondary};
 `;
 
+const Anchor = styled(Link)`
+  color: ${p => p.theme.colors.secondary};
+  border-bottom: 1px solid ${p => p.theme.colors.secondary};
+  margin-left: 6px;
+
+  &:hover,
+  &:focus {
+    color: ${p => p.theme.colors.accent};
+    border-bottom-color: ${p => p.theme.colors.accent};
+  }
+`;
