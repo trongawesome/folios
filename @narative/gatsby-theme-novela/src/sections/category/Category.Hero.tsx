e38b0a699d7d6edbdb92bf74fd65a968be9cf5de
tantrongtt <tantrongtt@gmail.com>
2020-08-20 19:17:58 +0800

    remove

--- @narative/gatsby-theme-novela/src/sections/category/Category.Hero.tsx
+++ @narative/gatsby-theme-novela/src/sections/category/Category.Hero.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import React from 'react'
 import styled from '@emotion/styled';
 
 import Section from '@components/Section';
